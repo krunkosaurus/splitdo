a007c79b55b666515e8433583a5c8000268cc4e1
Mauvis Ledford <mauvis@brainswap.me>
2015-03-24 11:52:43 -0700

    Support for surfacing errors.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function splitDo(origAr, splitBy, cb){
   // Check for existence of done callback to see if this should work non-blocking.
   var hasDoneCallback = cb.length > 1;
   // Array of split orig array.
+
   var splitArrays = [];
   var jobs = [];
 
@@ -20,22 +21,20 @@ function splitDo(origAr, splitBy, cb){
     splitArrays.forEach(function(arSection, i, all){
       jobs.push(function(){
         var d = Q.defer();
-        cb(arSection, function(){
-          d.resolve();
 
-          // Check to see if all jobs are done.
-          if (i + 1 == all.length){
-            deferred.resolve();
-          }
-        });
+        cb(arSection, d.resolve);
         return d.promise;
       });
     });
 
     var result = Q();
+
     jobs.forEach(function (f) {
       result = result.then(f);
     });
+    result
+      .fin(deferred.resolve)
+      .done();
 
     return deferred.promise;
   }else{
